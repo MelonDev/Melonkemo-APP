9875008ab91e6b31074e27b212c2bd62f749485f
Chaiwiwat <melondev@icloud.com>
2023-06-12 21:17:32 +0700

    Updated Page

--- lib/core/components/dialog_component.dart
+++ lib/core/components/dialog_component.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:melonkemo/core/components/bouncing/melon_bouncing_button.dart';
 import 'package:melonkemo/core/extensions/color_extension.dart';
-import 'package:shimmer/shimmer.dart';
 
 class DialogComponent {
   loadingSimple() {
@@ -30,8 +29,7 @@ class DialogComponent {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          SpinKitThreeBounce(
-                              size: 40, color: Colors.white),
+                          SpinKitThreeBounce(size: 40, color: Colors.white),
                           SizedBox(
                             height: 30,
                           ),
@@ -54,20 +52,6 @@ class DialogComponent {
     );
   }
 
-  test2() {
-    BotToast.showCustomLoading(
-        toastBuilder: (CancelFunc cancelFunc) {
-          return Container(
-              decoration: BoxDecoration(
-                  color: const Color(0x00F0F0F0).withOpacity(1),
-                  borderRadius: BorderRadius.circular(14)),
-              height: 70,
-              width: 70,
-              child: const SpinKitThreeBounce(size: 40, color: Colors.black));
-        },
-        backgroundColor: Colors.black);
-  }
-
   notify(String value, {Color? color, Color? onColor}) {
     BotToast.cleanAll();
     BotToast.showSimpleNotification(
@@ -78,69 +62,103 @@ class DialogComponent {
         closeIcon: Icon(Icons.cancel, color: onColor ?? Colors.white));
   }
 
-  test(String value) {
-    Color textColor = Colors.redAccent.darken(.4);
+  dialog(String value, {Color? color, bool fullscreenMode = false}) {
     BotToast.showWidget(
       toastBuilder: (CancelFunc cancelFunc) {
         return GestureDetector(
           onTap: cancelFunc,
           child: Container(
-            color: textColor.withOpacity(0.6),
+            //color: textColor.withOpacity(0.6),
             child: Center(
-                child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: Material(
-                      color: Colors.transparent,
-                      child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.85),
-                              borderRadius: BorderRadius.circular(16),
-                              boxShadow: [
-                                BoxShadow(
-                                    color:
-                                        Colors.red.darken(.3).withOpacity(0.3),
-                                    blurRadius: 20)
-                              ]),
-                          width: 300,
-                          padding: const EdgeInsets.only(
-                              left: 16, top: 14, right: 16, bottom: 14),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("แจ้งเตือน",
-                                  style: TextStyle(
-                                    color: textColor,
-                                    fontSize: 22,
-                                    fontFamily: 'Itim',
-                                    fontWeight: FontWeight.w900,
-                                  )),
-                              const SizedBox(height: 6),
-                              Text(value,
-                                  style: TextStyle(
-                                    color: textColor.withOpacity(0.6),
-                                    fontSize: 16,
-                                    fontFamily: 'Itim',
-                                    fontWeight: FontWeight.w900,
-                                  )),
-                              const SizedBox(height: 20),
-                              Container(
-                                height: 1,
-                                color: textColor.withOpacity(0.1),
-                              ),
-                              const SizedBox(height: 16),
-                              MelonBouncingButton.text(
-                                  enabledHover: true,
-                                  text: "ปิด",
-                                  textColor: textColor,
-                                  callback: cancelFunc)
-                            ],
-                          )),
-                    ))),
+                child: _dialog(value,
+                    fullscreenMode: fullscreenMode,
+                    color: color,
+                    cancelFunc: cancelFunc)),
           ),
         );
       },
+    );
+  }
+
+  Widget _blurDialog(String value,
+      {Color? color, required CancelFunc cancelFunc}) {
+    Color themeColor = color ?? Colors.black;
+    Color onColor = themeColor.darken(.3);
+    return BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.75),
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(width: 2, color: themeColor.darken(.3))),
+              width: 300,
+              padding: const EdgeInsets.only(
+                  left: 16, top: 14, right: 16, bottom: 14),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 6),
+                  Text("แจ้งเตือน",
+                      style: TextStyle(
+                        color: onColor,
+                        fontSize: 24,
+                        fontFamily: 'Itim',
+                        fontWeight: FontWeight.w900,
+                      )),
+                  const SizedBox(height: 26),
+                  Text(value,
+                      style: TextStyle(
+                        color: onColor.withOpacity(0.8),
+                        fontSize: 18,
+                        fontFamily: 'Itim',
+                        fontWeight: FontWeight.normal,
+                      )),
+                  const SizedBox(height: 40),
+                  Container(
+                    height: 2,
+                    color: onColor.withOpacity(0.2),
+                  ),
+                  const SizedBox(height: 16),
+                  MelonBouncingButton.text(
+                      enabledHover: true,
+                      text: "ปิด",
+                      color: onColor,
+                      fontSize: 16,
+                      textColor: Colors.white,
+                      callback: cancelFunc)
+                ],
+              )),
+        ));
+  }
+
+  Widget _dialog(String value,
+      {bool fullscreenMode = false,
+      Color? color,
+      required CancelFunc cancelFunc}) {
+    Color themeColor = color ?? Colors.black;
+    Widget childWidget =
+        _blurDialog(value, color: color, cancelFunc: cancelFunc);
+
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+                color: themeColor.darken(.3).withOpacity(0.2),
+                blurRadius: 20,
+                spreadRadius: 10)
+          ]),
+      child: fullscreenMode
+          ? childWidget
+          : ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: childWidget,
+            ),
     );
   }
 
