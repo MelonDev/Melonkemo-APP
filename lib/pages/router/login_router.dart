3f0908c25e3f29e34044fbe8cd523188af7563ef
Chaiwiwat <melondev@icloud.com>
2023-06-25 04:07:53 +0700

    Updated Page

--- lib/pages/router/login_router.dart
+++ lib/pages/router/login_router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:layout/layout.dart';
-import 'package:melonkemo/pages/mobile/login_mobile_page.dart';
-import 'package:melonkemo/pages/tablet/login_tablet_page.dart';
+import 'package:melonkemo/pages/login/login_mobile_page.dart';
+import 'package:melonkemo/pages/login/login_tablet_page.dart';
 
 class LoginRouter extends StatelessWidget {
   const LoginRouter({Key? key}) : super(key: key);
