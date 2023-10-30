f0d11a5349223c930a899a21725aa05409125e58
Chaiwiwat Koksantia <chaiwiwat.k@ayodiacompany.com>
2023-10-30 15:39:38 +0700

    30 Oct 2023

--- lib/pages/me/redirect_me_page.dart
+++ lib/pages/me/redirect_me_page.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'dart:js' as js;
-
-class RedirectMePage extends StatelessWidget {
-  const RedirectMePage({Key? key}) : super(key: key);
-
-  void openInWindow() {
-    if (kIsWeb) {
-      js.context.callMethod('open', ['https://melonkemo.carrd.co', '_self']);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    openInWindow();
-    return Container();
-  }
-}
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'dart:js' as js;
+//
+// class RedirectMePage extends StatelessWidget {
+//   const RedirectMePage({Key? key}) : super(key: key);
+//
+//   void openInWindow() {
+//     if (kIsWeb) {
+//       js.context.callMethod('open', ['https://melonkemo.carrd.co', '_self']);
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     openInWindow();
+//     return Container();
+//   }
+// }
