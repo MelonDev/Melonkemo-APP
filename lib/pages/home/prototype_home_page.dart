27e734af0c4838fe7d9051d9b33cf4b8ce944f78
Chaiwiwat <melondev@icloud.com>
2024-02-19 01:36:14 +0700

    Updated Page

--- lib/pages/home/prototype_home_page.dart
+++ lib/pages/home/prototype_home_page.dart
@@ -24,21 +24,6 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
 
   double blur = 0;
 
-  @override
-  void initState() {
-    _controller.addListener(() {
-      print("SIZE: ${_controller.size}");
-      setState(() {
-        blur = (_controller.size <= 0.25)
-            ? 0.0
-            : (_controller.size >= 0.55)
-                ? 2.0
-                : (_controller.size - 0.25) * (2.0 / 0.3);
-      });
-    });
-    super.initState();
-  }
-
   final LayoutValue<double> realCardWidth = LayoutValue.builder((layout) {
     return layout.width;
   });
@@ -51,10 +36,13 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return MelonScaffoldWidget(
-      body: _layout(context).animate().fadeIn(delay: const Duration(milliseconds: 300)).move(
-          delay: const Duration(milliseconds: 500),
-          begin: const Offset(0, 100),
-          end: const Offset(0, 0)),
+      body: _layout(context)
+          .animate()
+          .fadeIn(delay: const Duration(milliseconds: 300))
+          .move(
+              delay: const Duration(milliseconds: 500),
+              begin: const Offset(0, 100),
+              end: const Offset(0, 0)),
       children: [_background(context)],
     );
   }
@@ -73,7 +61,8 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
     double topPadding = MediaQuery.of(context).size.height * 0.7;
     return _listCards(context,
         topPadding: topPadding,
-        contentWidth: MediaQuery.of(context).size.width,
+        //contentWidth: cardWidth.resolve(context),
+        contentWidth: realCardWidth.resolve(context),
         mainAxisAlignment: MainAxisAlignment.center);
   }
 
@@ -81,14 +70,14 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
     double topPadding = MediaQuery.of(context).size.height * 0.7;
     return _listCards(context,
         topPadding: topPadding,
-        contentWidth: MediaQuery.of(context).size.width,
+        contentWidth: realCardWidth.resolve(context),
         mainAxisAlignment: MainAxisAlignment.center);
   }
 
   Widget _largeLayout(BuildContext context) {
     print("width: ${MediaQuery.of(context).size.width}");
-    double extraWidth = MediaQuery.of(context).size.width > 1050
-        ? (MediaQuery.of(context).size.width - 1050) / 1.45
+    double extraWidth = realCardWidth.resolve(context) > 1050
+        ? (realCardWidth.resolve(context) - 1050) / 1.45
         : 0;
     print("extraWidth: $extraWidth, right: ${20 + extraWidth}");
 
@@ -100,108 +89,125 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
       double topPadding = 60,
       double? contentWidth,
       MainAxisAlignment? mainAxisAlignment}) {
+    Widget listContainer = Container(
+      width: cardWidth.resolve(context),
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: DraggableScrollableSheet(
+          //controller: _controller,
+          expand: false,
+          initialChildSize: 1.0,
+          minChildSize: 1.0,
+          maxChildSize: 1.0,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return ListView(
+              controller: scrollController,
+              physics: const BouncingScrollPhysics(),
+              padding: EdgeInsets.only(top: topPadding, bottom: 60),
+              children: [
+                if (MediaQuery.of(context).size.width <= 1036)
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Shimmer.fromColors(
+                        baseColor: Colors.red,
+                        highlightColor: Colors.yellow,
+                        direction: ShimmerDirection.btt,
+                        child: Text(
+                          'Shimmer',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontFamily: 'Itim',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                      // child: Shimmer(
+                      //   gradient: LinearGradient(colors: [
+                      //     Colors.white.withOpacity(0.4),
+                      //     Colors.white.withOpacity(1.0)
+                      //   ]),
+                      //   child: const Row(
+                      //     mainAxisSize: MainAxisSize.max,
+                      //     mainAxisAlignment: MainAxisAlignment.center,
+                      //     crossAxisAlignment: CrossAxisAlignment.center,
+                      //     children: [
+                      //       RotatedBox(
+                      //           quarterTurns: 1,
+                      //           child: Icon(
+                      //             CupertinoIcons.chevron_left_2,
+                      //             size: 18,
+                      //           )),
+                      //       SizedBox(width: 8),
+                      //       Text(
+                      //         'เลื่อนขึ้นเพื่อดูข้อมูล',
+                      //         textAlign: TextAlign.center,
+                      //         style: TextStyle(
+                      //           fontSize: 18.0,
+                      //           fontFamily: 'Itim',
+                      //           fontWeight: FontWeight.bold,
+                      //         ),
+                      //       ),
+                      //       SizedBox(width: 18),
+                      //     ],
+                      //   ),
+                      // ),
+                      ),
+                CardWidget(
+                  cardTitleColor: Colors.white.withOpacity(.0),
+                  cardColor: Colors.grey.withOpacity(.48),
+                  titleColor: Colors.white.withOpacity(.9),
+                  width: cardWidth,
+                  sigma: 32,
+                  betweenBottom: 0,
+                  title: "ทดสอบ 1",
+                  children: [Container(height: 100)],
+                ),
+                CardWidget(
+                  cardTitleColor: Colors.white.withOpacity(.0),
+                  cardColor: Colors.grey.withOpacity(.48),
+                  titleColor: Colors.white.withOpacity(.9),
+                  sigma: 32,
+                  width: cardWidth,
+                  betweenBottom: 0,
+                  title: "ทดสอบ 2",
+                  children: [Container(height: 200)],
+                ),
+                CardWidget(
+                  cardTitleColor: Colors.white.withOpacity(.0),
+                  cardColor: Colors.grey.withOpacity(.48),
+                  titleColor: Colors.white.withOpacity(.9),
+                  width: cardWidth,
+                  sigma: 32,
+                  betweenBottom: 0,
+                  title: "ทดสอบ 3",
+                  children: [Container(height: 150)],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+
     return Container(
       padding: EdgeInsets.only(left: 20, right: 20 + extraWidth),
-      width: contentWidth ?? MediaQuery.of(context).size.width - 550,
-      child: Row(
-        mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.end,
-        children: [
-          Container(
-            width: cardWidth.resolve(context),
-            child: ScrollConfiguration(
-              behavior:
-                  ScrollConfiguration.of(context).copyWith(scrollbars: false),
-              child: DraggableScrollableSheet(
-                controller: _controller,
-                expand: false,
-                initialChildSize: 1.0,
-                minChildSize: 1.0,
-                maxChildSize: 1.0,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return ListView(
-                    controller: scrollController,
-                    physics: const BouncingScrollPhysics(),
-                    padding: EdgeInsets.only(top: topPadding, bottom: 60),
-                    children: [
-                      if (MediaQuery.of(context).size.width <= 1036)
-                        Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: Shimmer(
-                              gradient: LinearGradient(colors: [
-                                Colors.white.withOpacity(0.4),
-                                Colors.white.withOpacity(1.0)
-                              ]),
-                              child: const Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  RotatedBox(
-                                      quarterTurns: 1,
-                                      child: Icon(
-                                        CupertinoIcons.chevron_left_2,
-                                        size: 18,
-                                      )),
-                                  SizedBox(width: 8),
-                                  Text(
-                                    'เลื่อนขึ้นเพื่อดูข้อมูล',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontFamily: 'Itim',
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(width: 18),
-                                ],
-                              ),
-                            ),),
-                      CardWidget(
-                        cardTitleColor: Colors.white.withOpacity(.0),
-                        cardColor: Colors.grey.withOpacity(.48),
-                        titleColor: Colors.white.withOpacity(.9),
-                        width: cardWidth,
-                        sigma: 32,
-                        betweenBottom: 0,
-                        title: "ทดสอบ 1",
-                        children: [Container(height: 100)],
-                      ),
-                      CardWidget(
-                        cardTitleColor: Colors.white.withOpacity(.0),
-                        cardColor: Colors.grey.withOpacity(.48),
-                        titleColor: Colors.white.withOpacity(.9),
-                        sigma: 32,
-                        width: cardWidth,
-                        betweenBottom: 0,
-                        title: "ทดสอบ 2",
-                        children: [Container(height: 200)],
-                      ),
-                      CardWidget(
-                        cardTitleColor: Colors.white.withOpacity(.0),
-                        cardColor: Colors.grey.withOpacity(.48),
-                        titleColor: Colors.white.withOpacity(.9),
-                        width: cardWidth,
-                        sigma: 32,
-                        betweenBottom: 0,
-                        title: "ทดสอบ 3",
-                        children: [Container(height: 150)],
-                      ),
-                    ],
-                  );
-                },
-              ),
+      width: contentWidth ?? realCardWidth.resolve(context) - 550,
+      child: realCardWidth.resolve(context) <= 600
+          ? listContainer
+          : Row(
+              mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.end,
+              children: [
+                listContainer
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 
   String _image(BuildContext context) {
-    if (MediaQuery.of(context).size.width > 1036) {
+    if (realCardWidth.resolve(context) > 1036) {
       return "assets/images/melon_pool_large.webp";
-    } else if (MediaQuery.of(context).size.width > 600) {
+    } else if (realCardWidth.resolve(context) > 600) {
       return "assets/images/melon_pool_medium.webp";
     } else {
       return "assets/images/melon_pool_small.webp";
@@ -218,7 +224,7 @@ class _PrototypeHomePageState extends State<PrototypeHomePage> {
       child: Image.asset(
         _image(context),
         fit: BoxFit.cover,
-        alignment: Alignment.centerRight,
+        alignment: realCardWidth.resolve(context) > 600 ? Alignment.centerRight : Alignment.center,
       ),
     );
 
