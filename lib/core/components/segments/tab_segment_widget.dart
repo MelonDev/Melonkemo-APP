b3d275bca71dde716bbff26525eb2847ef11f294
Chaiwiwat <melondev@icloud.com>
2024-02-23 02:10:13 +0700

    Updated Page

--- lib/core/components/segments/tab_segment_widget.dart
+++ lib/core/components/segments/tab_segment_widget.dart
@@ -60,7 +60,7 @@ class _TabSegmentWidgetState extends State<TabSegmentWidget> {
   Widget _tabBar(BuildContext context) {
     return Container(
       color: widget.backgroundColor ?? Colors.white.withOpacity(.28),
-      height: widget.borderRadius + 2,
+      //height: widget.borderRadius + 2,
       child: TabBar(
         controller: widget.controller,
         isScrollable: widget.isScrollable,
