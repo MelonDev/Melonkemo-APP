1ada3592287cc97fe9eaa14bffeb4edea7427678
Chaiwiwat <melondev@icloud.com>
2024-02-24 02:37:50 +0700

    Updated Page

---
+++ lib/pages/sushiro/dialogs/people_page_dialog.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:layout/layout.dart';
+import 'package:melonkemo/core/components/bouncing/melon_bouncing_button.dart';
+import 'package:melonkemo/pages/sushiro/sushiro_main_model.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
+
+
+WoltModalSheetPage peoplePage(
+    BuildContext modalSheetContext, TextTheme textTheme,PeopleModel people) {
+  final LayoutValue<Size> size = LayoutValue.builder((layout) {
+    return Size(layout.width, layout.size.height);
+  });
+
+  return WoltModalSheetPage(
+    hasSabGradient: false,
+    forceMaxHeight: false,
+    navBarHeight: 0,
+    backgroundColor: const Color(0xFFFFB920),
+    isTopBarLayerAlwaysVisible: false,
+    child: Container(
+      color: Colors.white,
+      height: size.resolve(modalSheetContext).width < 560
+          ? size.resolve(modalSheetContext).height * 0.92
+          : size.resolve(modalSheetContext).height * 0.85,
+      child: Column(
+        children: [
+          Container(
+            height: 56,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+
+            //color: Colors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MelonBouncingButton(
+                  callback: () {
+                    Navigator.of(modalSheetContext).pop();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Icon(
+                      CupertinoIcons.clear,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 46),
+                  child: Text(
+                    people.name,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Itim',
+                        color: Colors.black),
+                  ),
+                ),
+                MelonBouncingButton.text(
+                    text: "ยืนยัน",
+                    color: Colors.amberAccent,
+                    fontSize: 18,
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    borderRadius: 100)
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
--- lib/pages/sushiro/sushiro_main_page.dart
+++ lib/pages/sushiro/sushiro_main_page.dart
@@ -1,11 +1,23 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:layout/layout.dart';
+import 'package:melonkemo/core/components/bouncing/melon_bouncing_button.dart';
 import 'package:melonkemo/core/components/me/melon_scaffold_widget.dart';
+import 'package:melonkemo/core/extensions/widget_extension.dart';
+import 'package:melonkemo/pages/infrastructure/under_construction_page.dart';
+import 'package:melonkemo/pages/sushiro/dialogs/add_people_dialog.dart';
 import 'package:melonkemo/pages/sushiro/sushiro_main_model.dart';
 import 'package:melonkemo/pages/sushiro/sushiro_main_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
+
+import 'dialogs/people_page_dialog.dart';
+import 'dialogs/summary_page_dialog.dart';
+
+typedef WoltPageLayoutCallback = WoltModalSheetPage Function(
+    BuildContext context, TextTheme textTheme);
 
 class SushiroMainPage extends StatefulWidget {
   const SushiroMainPage({Key? key}) : super(key: key);
@@ -19,7 +31,6 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
 
   double tableWidth = 30;
   double bottomSheetHeight = 100;
-
 
   final LayoutValue<double> areaWidth = LayoutValue.builder((layout) {
     return layout.width <= 500 ? layout.width : 500;
@@ -50,61 +61,83 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
             ],
           ),
           buttonText: "เพิ่มคน",
-          onButtonClick: (){
-
+          onButtonClick: () {
+            showSmallDialog(
+                context,
+                pageHeight: 0.3,
+                (BuildContext modalSheetContext, TextTheme textTheme) =>
+                    addPeople(modalSheetContext, textTheme, pageHeight: 0.3));
           },
           bottomSheet: Container(
             color: Colors.white,
             child: Row(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  bottomSheet(ct)
-                ]
-            ),
+                children: [bottomSheet(ct)]),
           ),
         );
       },
     );
   }
-  
-  Widget bottomSheet(BuildContext context){
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(0.2),
-        borderRadius: const BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))
-      ),
-      width: areaWidth.resolve(context),
-      height: bottomSheetHeight,
-      padding: const EdgeInsets.only(left:20,right:20,bottom: 0,top:0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top:17),
-            child: Text(
-              "ราคารวม",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Stack(
-            alignment:Alignment.centerRight,
-            children: [
-              Text(
-                "${context.read<SushiroMainProvider>().calculatePrice(includeServiceCharge: true).toStringAsFixed(2)} บาท",
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold,letterSpacing: 0),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 56),
-                child: Text(
-                  "ราคาก่อนเซอร์วิส:  ${context.read<SushiroMainProvider>().calculatePrice().toStringAsFixed(2)} บาท",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+
+  Widget bottomSheet(BuildContext context) {
+    return MelonBouncingButton(
+      callback: () {
+        showDialog(
+            context,
+            (BuildContext modalSheetContext, TextTheme textTheme) =>
+                summaryPage(modalSheetContext, textTheme));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.2),
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        width: areaWidth.resolve(context),
+        height: bottomSheetHeight,
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 0, top: 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 17),
+              child: Text(
+                "ราคารวม",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Itim',
                 ),
               ),
-            ],
-          )
-        ],
+            ),
+            Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                Text(
+                  "${context.read<SushiroMainProvider>().calculatePrice(includeServiceCharge: true).toStringAsFixed(2)} บาท",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0,
+                    fontFamily: 'Itim',
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 56),
+                  child: Text(
+                    "ราคาก่อนเซอร์วิส:  ${context.read<SushiroMainProvider>().calculatePrice().toStringAsFixed(2)} บาท",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Itim',
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -116,7 +149,7 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
         behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
         child: ListView.separated(
           itemCount: peoples.length,
-          padding: EdgeInsets.only(bottom: bottomSheetHeight+50),
+          padding: EdgeInsets.only(bottom: bottomSheetHeight + 50),
           separatorBuilder: (BuildContext context, int index) => Container(
             width: 1,
             height: 1,
@@ -141,68 +174,101 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
         plates.whereType<SushiPlateModel>().toList(),
         (SushiPlateModel sushiPlate) => sushiPlate.type);
 
-    return Container(
-      padding: const EdgeInsets.only(bottom: 20, left: 16,right:16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top:17),
-                child: Text(
-                  people.name,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Stack(
-                alignment:Alignment.centerRight,
+    return MelonBouncingButton(
+      callback: () {
+        showDialog(
+            context,
+            (BuildContext modalSheetContext, TextTheme textTheme) =>
+                peoplePage(modalSheetContext, textTheme, people));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "${context.read<SushiroMainProvider>().calculatePrice(peopleId: people.id,includeServiceCharge: true).toStringAsFixed(2)} บาท",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,letterSpacing: 0),
-                  ),
                   Container(
-                    margin: const EdgeInsets.only(top: 42),
+                    margin: const EdgeInsets.only(top: 17),
                     child: Text(
-                      "ราคาก่อนเซอร์วิส:  ${context.read<SushiroMainProvider>().calculatePrice(peopleId: people.id,).toStringAsFixed(2)} บาท",
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+                      people.name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Itim',
+                      ),
                     ),
                   ),
+                  Stack(
+                    alignment: Alignment.centerRight,
+                    children: [
+                      Text(
+                        "${context.read<SushiroMainProvider>().calculatePrice(peopleId: people.id, includeServiceCharge: true).toStringAsFixed(2)} บาท",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0,
+                          fontFamily: 'Itim',
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 42),
+                        child: Text(
+                          "ราคาก่อนเซอร์วิส:  ${context.read<SushiroMainProvider>().calculatePrice(
+                                peopleId: people.id,
+                              ).toStringAsFixed(2)} บาท",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: 'Itim',
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              getCounterPlateWidget(SushiPlateType.copper,
-                  plates: groupSushiPlates[SushiPlateType.copper] ?? []),
-              const SizedBox(width: 16),
-              getCounterPlateWidget(SushiPlateType.silver,
-                  plates: groupSushiPlates[SushiPlateType.silver] ?? []),
-              const SizedBox(width: 16),
-              getCounterPlateWidget(SushiPlateType.gold,
-                  plates: groupSushiPlates[SushiPlateType.gold] ?? []),
-              const SizedBox(width: 16),
-              getCounterPlateWidget(SushiPlateType.black,
-                  plates: groupSushiPlates[SushiPlateType.black] ?? []),
-              const SizedBox(width: 16),
-              getCounterPlateWidget(null,
-                  plates: sideDishPlates)
-            ],
-          )
-        ],
-      ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+                height: 100,
+                child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    children: [
+                      getCounterPlateWidget(SushiPlateType.copper,
+                          plates:
+                              groupSushiPlates[SushiPlateType.copper] ?? []),
+                      const SizedBox(width: 16),
+                      getCounterPlateWidget(SushiPlateType.silver,
+                          plates:
+                              groupSushiPlates[SushiPlateType.silver] ?? []),
+                      const SizedBox(width: 16),
+                      getCounterPlateWidget(SushiPlateType.gold,
+                          plates: groupSushiPlates[SushiPlateType.gold] ?? []),
+                      const SizedBox(width: 16),
+                      getCounterPlateWidget(SushiPlateType.black,
+                          plates: groupSushiPlates[SushiPlateType.black] ?? []),
+                      const SizedBox(width: 16),
+                      getCounterPlateWidget(null, plates: sideDishPlates)
+                    ]))
+          ],
+        ),
+      ).hover(y: -5, x: -10),
     );
   }
 
-  Widget getCounterPlateWidget(SushiPlateType? type, {required List<PlateModel> plates}) {
-    double price = type != null ? SushiroMainProvider.calculateSushi(type,plates.length) : SushiroMainProvider.calculateSideDish(plates);
+  Widget getCounterPlateWidget(SushiPlateType? type,
+      {required List<PlateModel> plates}) {
+    double price = type != null
+        ? SushiroMainProvider.calculateSushi(type, plates.length)
+        : SushiroMainProvider.calculateSideDish(plates);
     return Column(
       children: [
         Stack(
@@ -214,17 +280,20 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
               style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Itim',
                   color: getSushiPlateTextColor(type)),
             )
           ],
         ),
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
         Text(
           "${price > 0 ? price : "-"}",
-
           style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
+              fontFamily: 'Itim',
               color: getSushiPlateBorderColor(type)),
         )
       ],
@@ -233,13 +302,17 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
 
   Widget getSushiPlateWidget(SushiPlateType? type) {
     if (type == SushiPlateType.copper) {
-      return plateWidget(getSushiPlateColor(type), borderColor: getSushiPlateBorderColor(type));
+      return plateWidget(getSushiPlateColor(type),
+          borderColor: getSushiPlateBorderColor(type));
     } else if (type == SushiPlateType.silver) {
-      return plateWidget(getSushiPlateColor(type), borderColor: getSushiPlateBorderColor(type));
+      return plateWidget(getSushiPlateColor(type),
+          borderColor: getSushiPlateBorderColor(type));
     } else if (type == SushiPlateType.gold) {
-      return plateWidget(getSushiPlateColor(type), borderColor: getSushiPlateBorderColor(type));
+      return plateWidget(getSushiPlateColor(type),
+          borderColor: getSushiPlateBorderColor(type));
     } else if (type == SushiPlateType.black) {
-      return plateWidget(getSushiPlateColor(type), borderColor: getSushiPlateBorderColor(type));
+      return plateWidget(getSushiPlateColor(type),
+          borderColor: getSushiPlateBorderColor(type));
     } else {
       return plateWidget(Colors.transparent,
           borderRadius: 14, borderColor: getSushiPlateBorderColor(type));
@@ -299,6 +372,63 @@ class _SushiroMainPageState extends State<SushiroMainPage> {
           borderRadius: BorderRadius.circular(borderRadius)),
       width: 60,
       height: 60,
+    );
+  }
+
+  void showDialog(BuildContext context, WoltPageLayoutCallback page) {
+    final LayoutValue<double> width = LayoutValue.builder((layout) {
+      return layout.width;
+    });
+
+    WoltModalSheet.show<void>(
+      //pageIndexNotifier: pageIndexNotifier,
+      barrierDismissible: true,
+      showDragHandle: false,
+      enableDrag: true,
+      context: context,
+      pageListBuilder: (modalSheetContext) {
+        final textTheme = Theme.of(context).textTheme;
+        return [page.call(context, textTheme)];
+      },
+      modalTypeBuilder: (context) {
+        if (width.resolve(context) < 560) {
+          return WoltModalType.bottomSheet;
+        } else {
+          return WoltModalType.dialog;
+        }
+      },
+      onModalDismissedWithBarrierTap: () {
+        Navigator.of(context).pop();
+      },
+      maxDialogWidth: 880,
+      minDialogWidth: areaWidth.resolve(context),
+      minPageHeight: width.resolve(context) < 560 ? 0.9 : 0.85,
+      maxPageHeight: 1.0,
+    );
+  }
+
+  void showSmallDialog(BuildContext context, WoltPageLayoutCallback page,
+      {double pageHeight = 0.4}) {
+    WoltModalSheet.show<void>(
+      //pageIndexNotifier: pageIndexNotifier,
+      barrierDismissible: true,
+      showDragHandle: false,
+      enableDrag: true,
+      context: context,
+      pageListBuilder: (modalSheetContext) {
+        final textTheme = Theme.of(context).textTheme;
+        return [page.call(context, textTheme)];
+      },
+      modalTypeBuilder: (context) {
+        return WoltModalType.dialog;
+      },
+      onModalDismissedWithBarrierTap: () {
+        Navigator.of(context).pop();
+      },
+      maxDialogWidth: 360,
+      minDialogWidth: 360,
+      minPageHeight: pageHeight,
+      maxPageHeight: pageHeight,
     );
   }
 }
