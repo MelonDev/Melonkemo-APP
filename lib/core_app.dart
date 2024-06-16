5cfb0317a21099855d6c01cfc95b22fb0bf422c0
MelonDev <melondev@icloud.com>
2024-06-16 07:54:57 +0700

    Updated

--- lib/core_app.dart
+++ lib/core_app.dart
@@ -51,7 +51,7 @@ class CoreApp extends BaseRouterWidget {
         url: 'https://1drv.ms/f/s!AgXh7wuvRh0Xi-MbGtfg2aqQY11jJg?e=xWnyBv'),
     SharedPage.route(
         path: 'smart-buff-manager',
-        url: 'https://drive.google.com/file/d/1g6iwU7uKradb2oHf_aexFd-MCP7eEZQk/view?usp=share_link'),
+        url: 'https://drive.google.com/file/d/1ziSaVnNcV4EFjTwE7_nHe-7FdO938mme/view?usp=share_link'),
   ];
 
   @override
