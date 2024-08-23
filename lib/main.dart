b5de234a2769e16a5f4301ee069678c604aac4e8
MelonDev <melondev@icloud.com>
2024-08-24 00:57:41 +0700

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:universal_html/html.dart';
 import 'core_app.dart';
 
 void main() {
@@ -10,5 +12,10 @@ void main() {
       systemNavigationBarColor: Colors.white.withOpacity(0.02),
       systemNavigationBarContrastEnforced: true,
       systemNavigationBarIconBrightness: Brightness.dark));
+
+  if(kIsWeb){
+    document.documentElement?.requestFullscreen();
+  }
+
   runApp(CoreApp());
 }
